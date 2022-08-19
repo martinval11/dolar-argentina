d7907685476dbdce64f4a48b1a9aa34fbea08e4d
martinval11 <100082171+martinval11@users.noreply.github.com>
2022-08-19 18:56:17 -0300

    Add files via upload

--- src/pages/Euro.js
+++ src/pages/Euro.js
@@ -126,9 +126,11 @@ const Euro = () => {
 
 	const numbers = [
 		0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-		21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36
+		21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36,
 	];
-	const listItems = numbers.map((numbers) => <RecoverData id={numbers} key={numbers} />);
+	const listItems = numbers.map((numbers) => (
+		<RecoverData id={numbers} key={numbers} />
+	));
 
 	return (
 		<>
