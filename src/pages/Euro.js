30381e43fab682d09ac75489b9dda7b4b4563c5e
martinval11 <100082171+martinval11@users.noreply.github.com>
2022-08-19 19:09:29 -0300

    Add files via upload

--- src/pages/Euro.js
+++ src/pages/Euro.js
@@ -4,59 +4,46 @@ import { useEffect } from 'react';
 import React from 'react';
 import Footer from '../components/Footer';
 import '../css/euro.css';
-
 const Euro = () => {
 	const getData = async () => {
 		const res = await axios.get(
 			'https://www.dolarsi.com/api/api.php?type=euro'
 		);
-
 		for (let i = 0; i < 37; i++) {
 			localStorage.setItem(`casaNombre${i}`, res.data[i]['casa']['nombre']);
 			localStorage.setItem(`casaCompra${i}`, res.data[i]['casa']['compra']);
 			localStorage.setItem(`casaVenta${i}`, res.data[i]['casa']['venta']);
 		}
 	};
-
 	getData();
-
 	const updateList = async () => {
 		const updateList = document.querySelector('#updateList');
-
 		if (updateList.getAttribute('data-tooltip')) {
 			updateList.removeAttribute('data-tooltip');
 		}
-
 		updateList.setAttribute('aria-busy', 'true');
 		updateList.innerText = 'Actualizando Precios...';
-
 		await getData();
-
 		updateList.setAttribute('aria-busy', 'false');
 		updateList.innerText = 'Actualizar Precios';
 		updateList.setAttribute(
 			'data-tooltip',
 			'Los Precios han sido actualizados'
 		);
-
 		setTimeout(() => {
 			updateList.removeAttribute('data-tooltip');
 		}, 3500);
 	};
-
 	const RecoverData = ({ id }) => {
 		const dataName = localStorage.getItem(`casaNombre${id}`);
 		const dataPurchase = localStorage.getItem(`casaCompra${id}`);
 		const dataSale = localStorage.getItem(`casaVenta${id}`);
-
 		if (localStorage.getItem('casaNombre0') !== null) {
 			if (dataPurchase !== 'No Cotiza' && dataSale !== 'No Cotiza') {
 				let convertDataToNumber = dataPurchase.replaceAll(',', '.');
 				let dataPurchaseConverted = parseFloat(convertDataToNumber);
-
 				let convertSaleDataToNumber = dataSale.replaceAll(',', '.');
 				let dataSaleConverted = parseFloat(convertSaleDataToNumber);
-
 				return (
 					<>
 						<tr>
@@ -71,7 +58,6 @@ const Euro = () => {
 			} else if (dataPurchase !== 'No Cotiza' && dataSale === 'No Cotiza') {
 				let convertDataToNumber = dataPurchase.replaceAll(',', '.');
 				let dataPurchaseConverted = parseFloat(convertDataToNumber);
-
 				return (
 					<>
 						<tr>
@@ -86,7 +72,6 @@ const Euro = () => {
 			} else if (dataPurchase === 'No Cotiza' && dataSale !== 'No Cotiza') {
 				let convertSaleDataToNumber = dataSale.replaceAll(',', '.');
 				let dataSaleConverted = parseFloat(convertSaleDataToNumber);
-
 				return (
 					<>
 						<tr>
@@ -113,7 +98,6 @@ const Euro = () => {
 			}
 		}
 	};
-
 	useEffect(() => {
 		if (localStorage.getItem('casaNombre0') !== null) {
 			document.querySelector('#loading').style.display = 'none';
@@ -123,15 +107,11 @@ const Euro = () => {
 			}, 2000);
 		}
 	}, []);
-
 	const numbers = [
 		0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-		21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36,
+		21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36
 	];
-	const listItems = numbers.map((numbers) => (
-		<RecoverData id={numbers} key={numbers} />
-	));
-
+	const listItems = numbers.map((numbers) => <RecoverData id={numbers} key={numbers} />);
 	return (
 		<>
 			<main className='container'>
@@ -147,7 +127,6 @@ const Euro = () => {
 					Cargando, Espera por favor...
 				</button>
 			</main>
-
 			<table role='grid' className='container'>
 				<thead>
 					<tr>
@@ -158,10 +137,8 @@ const Euro = () => {
 				</thead>
 				<tbody>{listItems}</tbody>
 			</table>
-
 			<Footer position='inherit' />
 		</>
 	);
 };
-
 export default Euro;
