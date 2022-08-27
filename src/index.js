3916db80c4ffcf54744ae5431d59cc5356de76a2
martinval11 <100082171+martinval11@users.noreply.github.com>
2022-08-27 16:29:22 -0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-/* eslint-disable */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Conversor from './pages/Conversor';
@@ -9,8 +10,10 @@ import Euro from './pages/Euro';
 import './css/style.css';
 import OpenedMenu from './components/OpenedMenu';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-	<BrowserRouter>
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
 		<OpenedMenu />
 		<Routes>
 			<Route path='/' element={<App />} />
@@ -19,3 +22,13 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 		</Routes>
 	</BrowserRouter>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
